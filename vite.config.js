0b6e519b8d3b8b467d46d8f611bc3a1af507ac54
salespaulo <salespaulo08@gmail.com>
2026-09-16 12:57:18 -0300

    fix(d.ts): Correção para gerar arquivo index.d.ts em dist
    A configuração coleta os arquivos gerados pelo gleam build --target javascript

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,21 @@
 
 import { defineConfig } from "vite"
 import { resolve } from "path"
+import dts from 'vite-plugin-dts';
 
 // plugins
 import gleam from "vite-plugin-gleam"
 import tailwind from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [gleam(), tailwind()],
+  plugins: [gleam(), tailwind(), dts({
+    // Aponta para a pasta onde os arquivos .d.ts do Gleam
+    include: ['build/dev/javascript/gbr_ui_storybook/**/*'],
+    // Evita criar subpastas desnecessárias (ex: dist/build/dev/...)
+    entryRoot: 'build/dev/javascript/gbr_ui_storybook',
+  })],
   build: {
     lib: {
-      // Aponta para o arquivo .mjs que o compilador do Gleam gerou
       entry: resolve(import.meta.dirname, 'src', 'index.js'),
       name: 'index',
       formats: ['es', 'umd'],
@@ -22,13 +27,6 @@ export default defineConfig({
           return `index.cjs`
         }
       }
-    },
-    rollupOptions: {
-      // Caso sua lib use pacotes externos, liste-os aqui para não embuti-los no bundle
-      external: [],
-      output: {
-        globals: {},
-      },
     },
   }
 })
